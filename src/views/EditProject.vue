a360baee6e91e6fb4c0be65b63da2880e3f0d43e
Cesar Millan <cesar@Cesars-MacBook-Air.local>
2022-02-21 13:57:46 -0600

    Project finished

---
+++ src/views/EditProject.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="handleUpdate">
+     <label>Title</label>
+     <input type="text" required v-model="title">
+     <label>Details</label>
+     <textarea required v-model="details"></textarea>
+     <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+   props: [ 'id' ],
+   data(){
+      return{
+         title: '',
+         details: '',
+         uri: 'http://localhost:3000/projects/' + this.id,
+         project: {},
+      }
+   },
+   mounted(){
+      fetch(this.uri)
+      .then(res => res.json())
+      .then(data => {
+         this.project = data
+         this.title = this.project.title
+         this.details = this.project.details
+      })
+   },
+   methods: {
+      handleUpdate(){
+         fetch(this.uri, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ title: this.title, details: this.details })
+         }).then(() => {
+            this.$router.push('/')
+         }).catch(err => console.log(err))
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>
